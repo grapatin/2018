84bd540647aa8da632c55109ea4b16f8365e9e3d
Per Grapatin <per.grapatin@glooko.com>
2020-12-11 22:32:59 +0100

    folder names changed

---
+++ 01-Chronal-Calibration/out/testinput.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test2_ex3Result = exports.puzzle2_ex3 = exports.test2_ex2Result = exports.puzzle2_ex2 = exports.test2_ex1Result = exports.puzzle2_ex1 = exports.test1_ex3Result = exports.puzzle1_ex3 = exports.test1_ex2Result = exports.puzzle1_ex2 = exports.test1_ex1Result = exports.puzzle1_ex1 = void 0;
+//First part
+exports.puzzle1_ex1 = '+1, +1, +1';
+exports.test1_ex1Result = 3;
+exports.puzzle1_ex2 = '+1, +1, -2';
+exports.test1_ex2Result = 0;
+exports.puzzle1_ex3 = '-1, -2, -3';
+exports.test1_ex3Result = -6;
+//Second part
+exports.puzzle2_ex1 = '+1, -1';
+exports.test2_ex1Result = 0;
+exports.puzzle2_ex2 = '+3, +3, +4, -2, -4';
+exports.test2_ex2Result = 10;
+exports.puzzle2_ex3 = '-6, +3, +8, +5, -6';
+exports.test2_ex3Result = 5;
+//# sourceMappingURL=testinput.js.map
